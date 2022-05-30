75791615f6a4f16fd71505e6d12daccc36902c32
guilherme <guile.hm@hotmail.com>
2022-05-30 20:39:28 -0300

    update register component

--- src/components/Register/Register.tsx
+++ src/components/Register/Register.tsx
@@ -1,5 +1,22 @@
+import { InputLabel, FormControl, Input, FormHelperText } from "@mui/material"
+import { Container } from "@mui/system"
+
+
 const Register = () => {
-  return (<div>Hello from Register</div>)
+  return (
+    <Container>
+      <FormControl fullWidth={true}>
+        <InputLabel htmlFor="email">email</InputLabel>
+        <Input id="email" aria-describedby="email" />
+        <FormHelperText id="email-helper">{"we'll never share your email."}</FormHelperText>
+      </FormControl>
+      <FormControl fullWidth={true}>
+        <InputLabel htmlFor="password">password</InputLabel>
+        <Input id="password" aria-describedby="password" type="password" />
+        <FormHelperText id="password-helper">your secure password</FormHelperText>
+      </FormControl>
+    </Container>
+  )
 }
 
 
