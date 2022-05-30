d36f5c9a63080411a06b7998affbe6fa05c2203e
guilherme <guile.hm@hotmail.com>
2022-05-30 19:39:28 -0300

    add css baseline

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import './App.css'
-import Layout from './templates/Layout'
+import "./App.css"
+import Layout from "./templates/Layout"
+import CssBaseline from "@mui/material/CssBaseline"
 
 
 const App = () => {
   return (
     <div className="App">
+      <CssBaseline />
       <Layout>
         <div>hello</div>
       </Layout>
