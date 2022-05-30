e08bb170477338b7172ddf879912297a35a6ee02
guilherme <guile.hm@hotmail.com>
2022-05-30 20:48:28 -0300

    update variant

--- src/components/Register/Register.tsx
+++ src/components/Register/Register.tsx
@@ -5,12 +5,12 @@ import { Container } from "@mui/system"
 const Register = () => {
   return (
     <Container>
-      <FormControl fullWidth={true}>
+      <FormControl fullWidth={true} variant={"standard"}>
         <InputLabel htmlFor="email">email</InputLabel>
         <Input id="email" aria-describedby="email" />
         <FormHelperText id="email-helper">{"we'll never share your email."}</FormHelperText>
       </FormControl>
-      <FormControl fullWidth={true}>
+      <FormControl fullWidth={true} variant={"standard"}>
         <InputLabel htmlFor="password">password</InputLabel>
         <Input id="password" aria-describedby="password" type="password" />
         <FormHelperText id="password-helper">your secure password</FormHelperText>
