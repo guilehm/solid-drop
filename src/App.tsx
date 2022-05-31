7107ae5b4b7d05ad4ba7b9f714f0cc739bc86dc8
guilherme <guile.hm@hotmail.com>
2022-05-31 17:36:32 -0300

    remove material

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,7 @@
-import Layout from "./templates/Layout"
-import CssBaseline from "@mui/material/CssBaseline"
-import Steps from "./components/Steps"
-
-
 const App = () => {
   return (
     <div className="App">
-      <CssBaseline />
-      <Layout>
-        <Steps />
-      </Layout>
+      hello world
     </div>
   )
 }
--- src/components/Register/Register.tsx
+++ src/components/Register/Register.tsx
@@ -1,21 +1,6 @@
-import { InputLabel, FormControl, Input, FormHelperText } from "@mui/material"
-import { Container } from "@mui/system"
-
-
 const Register = () => {
   return (
-    <Container>
-      <FormControl fullWidth={true} variant={"standard"}>
-        <InputLabel htmlFor="email">email</InputLabel>
-        <Input id="email" aria-describedby="email" />
-        <FormHelperText id="email-helper">{"we'll never share your email."}</FormHelperText>
-      </FormControl>
-      <FormControl fullWidth={true} variant={"standard"}>
-        <InputLabel htmlFor="password">password</InputLabel>
-        <Input id="password" aria-describedby="password" type="password" />
-        <FormHelperText id="password-helper">your secure password</FormHelperText>
-      </FormControl>
-    </Container>
+    <div>Hello from Register</div>
   )
 }
 
