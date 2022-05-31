0cb152d07bf907c4c0009fa9e7f31a2650966df5
guilherme <guile.hm@hotmail.com>
2022-05-31 18:35:20 -0300

    update global style

--- src/global-styles.ts
+++ src/global-styles.ts
@@ -2,7 +2,8 @@ import { createGlobalStyle } from "styled-components"
 
 
 const GlobalStyle = createGlobalStyle`
-  body {
+  * {
+    box-sizing: border-box;
     margin: 0;
     padding: 0;
     font-family: 'Raleway', sans-serif;
