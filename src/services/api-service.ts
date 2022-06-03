735d8ff6df9be3add55590c080e5641a43376b08
guilherme <guile.hm@hotmail.com>
2022-06-03 18:24:11 -0300

    create api service

---
+++ src/services/api-service.ts
@@ -0,0 +1,34 @@
+import axios, { AxiosInstance } from "axios"
+
+
+const REACT_APP_API_URL = process.env.REACT_APP_API_URL || "http://localhost:8080"
+
+
+type UserCreateData = {
+  name?: string
+  email: string
+  password: string
+}
+
+class ApiService {
+  url: string
+  client: AxiosInstance
+
+  constructor(url: string = REACT_APP_API_URL) {
+    this.url = url
+    this.client = this.getClient()
+  }
+
+  getClient() {
+    const client = axios.create()
+    return client
+  }
+
+  createUser(data: UserCreateData) {
+    return this.client.post(`${this.url}/users/`, data)
+  }
+
+}
+
+
+export default ApiService
