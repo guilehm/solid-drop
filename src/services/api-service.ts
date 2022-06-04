74193cf7f51d60878a36464abd1f774d8cd33f1a
guilherme <guile.hm@hotmail.com>
2022-06-04 11:46:22 -0300

    update types

--- src/services/api-service.ts
+++ src/services/api-service.ts
@@ -10,6 +10,14 @@ type UserCreateData = {
   password: string
 }
 
+export type RegisterResponseData = {
+  id: number
+  token: string
+  refresh_token: string
+  email?: string
+  date_joined: string
+}
+
 class ApiService {
   url: string
   client: AxiosInstance
@@ -24,11 +32,11 @@ class ApiService {
     return client
   }
 
-  createUser(data: UserCreateData) {
+  createUser(data: UserCreateData): Promise<AxiosResponse<RegisterResponseData>> {
     return this.client.post(`${this.url}/users/`, data)
   }
 
-  validateUsername(username: string): Promise<AxiosResponse | AxiosError> {
+  validateUsername(username: string): Promise<AxiosResponse> {
     return this.client.post(`${this.url}/users/validate/username/`, { username })
   }
 
