4d0e56485a52a6e5e1fd181aa7e1e46423709892
guilherme <guile.hm@hotmail.com>
2022-06-04 15:25:49 -0300

    install react router dom and set register and login routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,39 @@
 import Container from "./components/Container"
 import Register from "./components/Register"
 import GlobalStyle from "./global-styles"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
 
 import "react-toastify/dist/ReactToastify.css"
+import Login from "./components/Login"
 
 const App = () => {
   return (
     <div className="App">
       <GlobalStyle />
-      <Container>
-        <Register />
-      </Container>
+      <BrowserRouter>
+        <Routes>
+
+          <Route path="/register" element={
+            <Container>
+              <Register />
+            </Container>
+          } />
+
+          <Route path="/login" element={
+            <Container>
+              <Login />
+            </Container>
+          } />
+
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
 
 export default App
+
+
+
+
