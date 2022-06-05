19cdb0bc5f549fac0f5965d3349a0dcafd43ebd7
guilherme <guile.hm@hotmail.com>
2022-06-05 14:36:38 -0300

    create home styles

---
+++ src/components/Home/Home.styles.ts
@@ -0,0 +1,17 @@
+import styled from "styled-components"
+
+export const Title = styled.h1`
+  font-size: 4rem;
+  font-weight: 300;
+  line-height: 60px;
+  padding-bottom: 20px;
+  margin: 0;
+`
+
+export const List = styled.ul`
+  display: flex;
+
+  & button {
+    margin-right: 5px;
+  }
+`
