decc2ab358354e609b5d4ca29485a658412abd4c
guilherme <guile.hm@hotmail.com>
2022-06-11 11:38:46 -0300

    add onclick property to button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -5,14 +5,18 @@ type ButtonProps = {
   color?: string
   bgColor?: string
   margin?: string
+  onClick?: React.MouseEventHandler<HTMLElement>
 }
 
 
-const Button: React.FC<ButtonProps> = ({ children, margin, color, bgColor }): JSX.Element => (
+const Button: React.FC<ButtonProps> = ({
+  children, margin, color, bgColor, onClick,
+}): JSX.Element => (
   <S.Button
     margin={margin}
     color={color}
-    bgColor={bgColor}>
+    bgColor={bgColor}
+    onClick={onClick}>
     {children}
   </S.Button>
 )
