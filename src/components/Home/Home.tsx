4c63369f596cd18f3009b0fbc3e2637ebe860fff
guilherme <guile.hm@hotmail.com>
2022-06-11 11:40:05 -0300

    add conditions to buttons

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -3,7 +3,18 @@ import Container from "../Container"
 import * as S from "./Home.styles"
 
 import { Link } from "react-router-dom"
+import { useState } from "react"
+import { useCookies } from "react-cookie"
+import { JWT_SECRET_KEY } from "../../settings"
 
+import { useJwt } from "react-jwt"
+
+
+type UserData = {
+  id: number
+  username: string
+  exp: number
+}
 
 const Home = () => {
 
@@ -11,20 +22,31 @@ const Home = () => {
 
   const { token } = cookies
   const { decodedToken, isExpired } = useJwt<UserData>(token)
+
   return (
     <Container>
       <S.Title>rogue like</S.Title>
+      {!isExpired && <S.Subtitle>Welcome {decodedToken?.username}!</S.Subtitle>}
       <S.List>
 
         <Button bgColor="green">start</Button>
 
-        <Link to="/login">
-          <Button>login</Button>
-        </Link>
+        {!isExpired &&
+          <Button
+            bgColor="red"
+            onClick={() => { removeCookie("token"), removeCookie("refresh") }}>
+            logout
+          </Button>}
 
-        <Link to="register">
-          <Button>register</Button>
-        </Link>
+        {isExpired &&
+          <Link to="login">
+            <Button>login</Button>
+          </Link>}
+
+        {isExpired &&
+          <Link to="register">
+            <Button>register</Button>
+          </Link>}
 
       </S.List>
     </Container >
