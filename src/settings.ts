1a2a8bc2af3c958a8eaa437a7fe3093501fc11bc
guilherme <guile.hm@hotmail.com>
2022-06-11 12:30:39 -0300

    fix tokens lifetimes

--- src/settings.ts
+++ src/settings.ts
@@ -1,2 +1,2 @@
-export const ACCESS_TOKEN_LIFETIME = 60 * 10
-export const REFRESH_TOKEN_LIFETIME = 60 * 24
+export const ACCESS_TOKEN_LIFETIME = 60 * 5
+export const REFRESH_TOKEN_LIFETIME = 60 * 60 * 24
