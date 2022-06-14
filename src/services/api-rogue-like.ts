bc32d193a22bfdb333a42fa7c99284052bc072c4
guilherme <guile.hm@hotmail.com>
2022-06-13 23:14:59 -0300

    create api rogue like

---
+++ src/services/api-rogue-like.ts
@@ -0,0 +1,21 @@
+import axios, { AxiosInstance, AxiosResponse } from "axios"
+import { REACT_APP_API_ROGUE_URL } from "../settings"
+import { Sprite } from "../types/api-types"
+
+
+class ApiRogueLike {
+  url: string
+  client: AxiosInstance
+
+  constructor(url: string = REACT_APP_API_ROGUE_URL) {
+    this.url = url
+    this.client = axios.create()
+  }
+
+  getSpriteList(): Promise<AxiosResponse<Array<Sprite>>> {
+    return this.client.get(`${this.url}/sprites/`)
+  }
+
+}
+
+export default ApiRogueLike
